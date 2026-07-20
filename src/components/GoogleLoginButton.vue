52902f6f348a2307f0c2f221574c672d958efd98
EDGAR-BRI <edgardev26@gmail.com>
2026-07-20 09:44:15 -0400

    fix: stop sending redirect_uri query param to backend

--- src/components/GoogleLoginButton.vue
+++ src/components/GoogleLoginButton.vue
@@ -4,8 +4,7 @@ import { PUBLIC_API_URL } from 'astro:env/client'
 const API_BASE = PUBLIC_API_URL
 
 function handleGoogleLogin() {
-  const callbackUrl = `${window.location.origin}/auth/callback`
-  window.location.href = `${API_BASE}/auth/google/redirect?redirect_uri=${encodeURIComponent(callbackUrl)}`
+  window.location.href = `${API_BASE}/auth/google/redirect`
 }
 </script>
 
